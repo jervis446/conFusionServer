7b0b538991e09b6ed956f5dc1d626fe0013e7877
jervis446 <kumar.adarshluv99@gmail.com>
2019-10-03 17:55:43 +0530

    files added

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
+//for passport
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
+
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
@@ -52,27 +57,23 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-  if(!req.session.user) {
+    if (!req.user) {
       var err = new Error('You are not authenticated!');
       err.status = 403;
-      return next(err);
-  }
-  else {
-    if (req.session.user === 'authenticated') {
-      next();
+      next(err);
     }
     else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+          next();
     }
-  }
 }
 
 
